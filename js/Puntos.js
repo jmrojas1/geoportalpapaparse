a4c3e5eb9344655ae47606a678ca7b668a4bbe8a
José Miguel <48037080+jmrojas1@users.noreply.github.com>
2022-02-26 16:37:07 -0300

    Update Puntos.js

--- js/Puntos.js
+++ js/Puntos.js
@@ -4,7 +4,7 @@ function init() {
     Papa.parse(sheetsUrlPuntos,{
          download: true,
          header: true,
-         complete: function(results, parser){
+         complete: function(results){
 			 var data = results.data
 			 console.log(data)
 		 }
