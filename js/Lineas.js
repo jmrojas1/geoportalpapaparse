8265ef328cf8de0ce75ea86c681be873a959245d
José Miguel <48037080+jmrojas1@users.noreply.github.com>
2022-02-26 15:38:20 -0300

    Update Lineas.js

--- js/Lineas.js
+++ js/Lineas.js
@@ -1,4 +1,4 @@
-var sheetsUrlLineas ="https://docs.google.com/spreadsheets/d/1JQUTHYFaVHT02ijiZ1O2GJWT6N14HlHhnRLLaa7gTSQ/edit#gid=0"
+var sheetsUrlLineas ="https://docs.google.com/spreadsheets/d/e/2PACX-1vQZvt58BuHAKch2AaUQsDc0Z-iQlvGIf5dZ0yrCEpWxiaJJt6wAVfFYvN4ZoCD5p5hie5eOWbFJ6JD1/pub?output=csv"
 
 function init() {
     Papa.parse(sheetsUrlLineas,{
